2925aee1bed679a3353937abc2423af0fccc522a
iamb4k <100809777+iamb4k@users.noreply.github.com>
2023-02-17 02:38:42 +0000

    Update SymmetricEncryption.cs

--- SymmetricEncryption.cs
+++ SymmetricEncryption.cs
@@ -1,5 +1,4 @@
 /*
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -12,9 +11,6 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
-
 */
 
 using System;
